6dc35654884c45792710b073db4693fab1fb6b7f
Tamas Kiss <kisstii1@gmail.com>
2020-12-10 09:31:43 +0100

    comments in js

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 function pageLoaded() {
 
+/*
+Todo applikációt készítünk, ami alapértelmezésben 10 üres mezőből áll, de ezt tudjuk bővíteni.
+Ki lehet pipálni az elvégzett feladatokat,de akár ki is lehet törölni azokat.
+*/
+
+//Változó a root-nak.
 
 
+//El kell tárolni a sorok számát (10).
+//Külön változóba egy sor struktúrájának tárolása (div, input, button stb)
 
 
+//for ciklus a soroknak és hozzáadjuk a root-hoz.
+
+
+//A root elemen kívül egy plusz gomb a lista végére (afterend).
+
+
+//Click esemény adása a gombnak. Új sort hoz létre.
+
+
+//Kijelölöm az összes törlés gombot class name alapján és eltárolom külön változóba.
+
+
+//for ciklus a click esemény rárakásához mindegyik törlés gombra.
 
 
 }
