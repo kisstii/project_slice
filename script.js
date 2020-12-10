1cd484c4f4f9bac6635689debfe2b60bdd7d5b23
Tamas Kiss <kisstii1@gmail.com>
2020-12-10 11:22:15 +0100

    after livecode

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
 function pageLoaded() {
 
-/*
-Todo applikációt készítünk, ami alapértelmezésben 10 üres mezőből áll, de ezt tudjuk bővíteni.
-Ki lehet pipálni az elvégzett feladatokat,de akár ki is lehet törölni azokat.
-*/
+	/*
+	Todo applikációt készítünk, ami alapértelmezésben 10 üres mezőből áll, de ezt tudjuk bővíteni.
+	Ki lehet pipálni az elvégzett feladatokat,de akár ki is lehet törölni azokat.
+	*/
 
-//Változó a root-nak.
+	//Változó a root-nak.
+	let root = document.querySelector(".root");
 
+	//El kell tárolni a sorok számát (10).
+	let numRows = 10;
+	
+	//Külön változóba egy sor struktúrájának tárolása (div, input, button stb)
+	let row = `
+		<div class="row">
+			<input type="checkbox">
+			<input type="text">
+			<button class="delete">delete</button>
+		</div>
+	`;
 
-//El kell tárolni a sorok számát (10).
-//Külön változóba egy sor struktúrájának tárolása (div, input, button stb)
+	//for ciklus a soroknak és hozzáadjuk a root-hoz.
+	for (let i = 0; i < numRows; i++) {
+		root.insertAdjacentHTML("beforeend", row);
+	}
 
+	//A root elemen kívül egy plusz gomb a lista végére (afterend).
+	root.insertAdjacentHTML("afterend", `
+		<button id="addNewRow">Add new row</button>
+	`);
 
-//for ciklus a soroknak és hozzáadjuk a root-hoz.
+	//Click esemény adása a gombnak. Új sort hoz létre.
+	function newItem() {
+		root.insertAdjacentHTML("beforeend", row);
 
+	//TODO: eseményfigyelőt rátenni az új "row"-ra
 
-//A root elemen kívül egy plusz gomb a lista végére (afterend).
+	}
 
+	document.getElementById("addNewRow").addEventListener("click", newItem)
 
-//Click esemény adása a gombnak. Új sort hoz létre.
+	//Kijelölöm az összes törlés gombot class name alapján és eltárolom külön változóba.
+	let removeBtns = root.querySelectorAll(".delete");
 
+	//for ciklus a click esemény rárakásához az összes törlés gombra.
+	function removeItem(b) {
+		b.target.parentElement.remove();
+	}
 
-//Kijelölöm az összes törlés gombot class name alapján és eltárolom külön változóba.
-
-
-//for ciklus a click esemény rárakásához mindegyik törlés gombra.
-
+	for (let i = 0; i < removeBtns.length; i++) {
+		removeBtns[i].addEventListener("click", removeItem);
+	}
 
 }
 
